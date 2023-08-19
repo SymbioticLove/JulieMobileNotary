cdcd3c4800ab4005bce9b4ff3814c5390c5e533d
Symbiotic Love <matthew@symbiotic.love>
2023-08-19 15:34:10 -0700

    Header animations
    Brief animations added to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,8 @@ const Header = () => {
   );
 
   const [isScrolled, setIsScrolled] = useState(false);
+  const [typedSubtitle, setTypedSubtitle] = useState('');
+  const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
 
   const handleScroll = () => {
     if (window.scrollY > 50) {
@@ -25,14 +27,35 @@ const Header = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (currentLetterIndex < headerSubtitle.length + 1) {
+      let typingTimer;
+
+      if (currentLetterIndex === 0) {
+        typingTimer = setTimeout(() => {
+          setCurrentLetterIndex(1);
+        }, 500);
+      } else {
+        typingTimer = setTimeout(() => {
+          setTypedSubtitle(headerSubtitle.substring(0, currentLetterIndex));
+          setCurrentLetterIndex(prevIndex => prevIndex + 1);
+        }, 100);
+      }
+
+      return () => clearTimeout(typingTimer);
+    }
+  }, [currentLetterIndex, headerSubtitle]);
+
   return (
     <div className={`header-container ${isScrolled ? 'shadow' : ''}`}>
-      <div className="logo">
-        <img src="https://placehold.co/150" alt="Logo" />
-      </div>
       <div className="title">
-        <h1>{headerTitle}</h1>
-        <p>{headerSubtitle}</p>
+        <div className="logo">
+          <img src="https://placehold.co/150" alt="RSS Logo" />
+        </div>
+        <div>
+          <h1>{headerTitle}</h1>
+          <p>{typedSubtitle}</p>
+        </div>
       </div>
       <div className="nav-menu"></div>
     </div>
