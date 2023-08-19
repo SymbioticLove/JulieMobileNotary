9f3d0a53cc2e9fb0f4160286ecee63488d08c76e
Symbiotic Love <matthew@symbiotic.love>
2023-08-19 14:58:56 -0700

    Testing complete
    Everything working as intended for development

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './Header.css';
 
@@ -6,18 +6,35 @@ const Header = () => {
   const { headerTitle, headerSubtitle } = useSelector(
     state => state.about.header,
   );
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="wrapper">
-      <div className="header-container">
-        <div className="logo">
-          <img src="https://placehold.co/150" />
-        </div>
-        <div className="title">
-          <h1>{headerTitle}</h1>
-          <p>{headerSubtitle}</p>
-        </div>
-        <div className="nav-menu"></div>
+    <div className={`header-container ${isScrolled ? 'shadow' : ''}`}>
+      <div className="logo">
+        <img src="https://placehold.co/150" alt="Logo" />
       </div>
+      <div className="title">
+        <h1>{headerTitle}</h1>
+        <p>{headerSubtitle}</p>
+      </div>
+      <div className="nav-menu"></div>
     </div>
   );
 };
