b0594ca706ddec705e10af4fb0635a887c125677
Symbiotic Love <matthew@symbiotic.love>
2023-08-19 18:53:15 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="wrapper">
       <Provider store={store}>
         <Header />
+        <Hero />
       </Provider>
     </div>
   );
